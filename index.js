5dbaca8d1facaed00354a6f4ce218fb63ce1067b
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-05-05 15:23:26 +0600

    loding data from mongodb

--- index.js
+++ index.js
@@ -17,8 +17,22 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.sk73k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db("electronic").collection("products");
+        app.get("/products",async(req,res)=>{
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir());
 
 app.listen(port, () => {
     console.log("connected");
